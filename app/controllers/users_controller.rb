195cc90d6bc2e1367e6423a3ec421744975a4732
Yumi Uematsu <yumi@Yumi-no-MacBook-Air.local>
2015-03-07 21:44:48 +0900

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)    # 実装は終わっていないことに注意!
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to sample app!"
       redirect_to @user
     else
